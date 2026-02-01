c7cf1652a4818c39907efb63f42ffc1a33dffbcd
APizzaDuck <norahbonskey@gmail.com>
2026-01-31 22:42:44 -0500

    concepts of a shooter

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.shooter;
 
 import static frc.robot.constants.ShooterConstants.*;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -10,6 +12,7 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -29,13 +32,16 @@ public class Shooter extends SubsystemBase{
 
     SparkFlex topFly = new SparkFlex(TOP_ROLLER_ID, MotorType.kBrushless);
     SparkFlexConfig topFlyConfig = new SparkFlexConfig();
+    RelativeEncoder topFlyEncoder = topFly.getEncoder();
 
    public Shooter() {
-    
+    configure();
    }
    
    @Override
-   public void periodic(){}
+   public void periodic(){
+    
+   }
 
    public void configure() {
     mainFly.setCANTimeout(250);
@@ -45,20 +51,47 @@ public class Shooter extends SubsystemBase{
     mainFlyConfig.idleMode(IdleMode.kBrake);
     
     mainFly.setCANTimeout(0);
+
+    topFly.setCANTimeout(250);
+
+    topFlyConfig.smartCurrentLimit(65);
+    topFlyConfig.inverted(false);
+    topFlyConfig.idleMode(IdleMode.kBrake);
+
+    topFly.setCANTimeout(0);
+
+
    }
 
    public void setFlywheelVoltage(double volts) {
     mainFly.setVoltage(volts);
+    Logger.recordOutput("Shooter/Flywheel/Volts", volts);
    }
    public void setTopRollerVoltage(double volts){
     topFly.setVoltage(volts);
+    Logger.recordOutput("Shooter/Roller/Volts", volts);
    }
    public void setFlywheelVelocity(){}
    public void setTopRollerVelocity() {}
 
-   public double getTopRollerVelocity() {}
-   public double getFlywheelVelocity() {}
+   public double getTopRollerVelocity() {
+    return topFlyEncoder.getVelocity();
+   }
+   public double getFlywheelVelocity() {
+    return mainFlyEncoder.getVelocity();
+   }
 
-   public double getFlywheelCurrent() {}
-   public double getTopRollerCurrent() {}
+   public double getFlywheelCurrent() {
+    return mainFly.getOutputCurrent();
+   }
+   public double getTopRollerCurrent() {
+    return topFly.getOutputCurrent();
+   }
+
+   public Command runFlywheel(double volts){
+    return run(() -> setFlywheelVoltage(12));
+   }
+   public Command runTopRoller(double volts){
+    return run(() -> setTopRollerVoltage(12));
+   }
 }
