0bdbca95aeebe8cb5cab96f17c20b1870eaf5efa
APizzaDuck <norahbonskey@gmail.com>
2026-02-16 18:34:04 -0500

    literally just the file for mechanism viz

---
+++ src/main/java/frc/robot/subsystems/MechanismViz.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+// does this even count as a subsystem honestly
+public class MechanismViz extends SubsystemBase {
+
+    Mechanism2d mech = new Mechanism2d(1,1);
+
+}
