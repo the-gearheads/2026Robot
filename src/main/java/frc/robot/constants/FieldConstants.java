73066fde172c0576cf6137d622d6e33570c56279
Ceticx0 <103392714+Ceticx0@users.noreply.github.com>
2026-03-03 20:07:06 -0500

    started trench auto retract code
    Co-authored-by: APizzaDuck <APizzaDuck@users.noreply.github.com>

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -64,8 +64,6 @@ public class FieldConstants {
     // Right of hub
     public static final double rightBumpStart = Hub.nearRightCorner.getY();
     public static final double rightBumpEnd = rightBumpStart - RightBump.width;
-    public static final double rightTrenchOpenStart = rightBumpEnd - Units.inchesToMeters(12.0);
-    public static final double rightTrenchOpenEnd = 0;
 
     // Left of hub
     public static final double leftBumpEnd = Hub.nearLeftCorner.getY();
