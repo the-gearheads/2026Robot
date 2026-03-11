a9f8f93663f6e0a5fcdd05a2b9c10bbcdd210418
person4268 <28717044+person4268@users.noreply.github.com>
2026-03-10 23:12:08 -0400

    add spline encoder sim, fixing it, or maybe it was something else, log integrated encoder position

--- src/main/java/frc/robot/subsystems/intake/IntakeSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSim.java
@@ -10,16 +10,17 @@ import com.revrobotics.sim.SparkMaxSim;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.util.SplineEncoderSim;
 
 public class IntakeSim extends Intake {
     DCMotor intakeGearbox = DCMotor.getNEO(1);
-    DCMotor deployGearbox = DCMotor.getNEO(1);
+    DCMotor deployGearbox = DCMotor.getNeoVortex(1);
     SparkMaxSim intakeFlexSim = new SparkMaxSim(intake, intakeGearbox);
     SparkFlexSim deployFlexSim = new SparkFlexSim(deploy, deployGearbox);
+    SplineEncoderSim deployEncoderSim = new SplineEncoderSim(deployEncoder);
     
     SingleJointedArmSim deploySim = new SingleJointedArmSim(
         LinearSystemId.createSingleJointedArmSystem(deployGearbox, 0.346396007, 60.0), 
@@ -27,23 +28,22 @@ public class IntakeSim extends Intake {
         // LinearSystemId.identifyPositionSystem(HOOD_FEEDFORWARD.getKv(), HOOD_FEEDFORWARD.getKa()),
         deployGearbox, 60.0, DEPLOY_LENGTH, DEPLOY_MIN_ANGLE.getRadians(), DEPLOY_MAX_ANGLE.getRadians(), false, 0);
 
-    FlywheelSim intakeSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(intakeGearbox, 0.0043895948, INTAKE_GEAR_RATIO), deployGearbox, 0);
-
-    public IntakeSim() {
-        configure();
-    }
+    FlywheelSim intakeSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(intakeGearbox, 0.0043895948, INTAKE_GEAR_RATIO), intakeGearbox, 0);
 
     @Override
     public void simulationPeriodic() {
+        
         intakeSim.setInputVoltage(intakeFlexSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
         intakeSim.update(0.02);
 
         deploySim.setInputVoltage(deployFlexSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
         deploySim.update(0.02);
+        
 
         deployFlexSim.iterate(deploySim.getVelocityRadPerSec(), RoboRioSim.getVInVoltage(), 0.02);  // velocity should be in post conversion units, so radians/sec of the Hood
+        deployEncoderSim.setPosition(deployFlexSim.getPosition());
+        deployEncoderSim.setAngle(deployFlexSim.getPosition());
+        deployEncoderSim.setVelocity(deployFlexSim.getVelocity());
         intakeFlexSim.iterate(intakeSim.getAngularVelocityRPM(), RoboRioSim.getVInVoltage(), 0.02);  // really this shjo
-
-        RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(deploySim.getCurrentDrawAmps() + intakeSim.getCurrentDrawAmps()));
     }
 }
---
+++ src/main/java/frc/robot/util/SplineEncoderSim.java
@@ -0,0 +1,178 @@
+package frc.robot.util;
+
+import com.revrobotics.encoder.DetachedEncoder;
+import edu.wpi.first.hal.SimBoolean;
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
+
+public class SplineEncoderSim {
+  private SimDouble m_position;
+  private SimDouble m_velocity;
+  private SimDouble m_angle;
+  private SimDouble m_rawAngle;
+  private SimBoolean m_isInverted;
+  private SimDouble m_zeroOffset;
+  private SimDouble m_positionConversionFactor;
+  private SimDouble m_velocityConversionFactor;
+  private int id;
+  private String simDeviceName;
+
+  /**
+   * Create a Spark Flex External Encoder simulation object for a Spark Flex. This will allow you to
+   * read/write data from the simulated sensor and view it in the Sim GUI.
+   *
+   * @param motor The CANSparkFlex associated with the sensor
+   */
+  public SplineEncoderSim(DetachedEncoder encoder) {
+    simDeviceName = "Detached MAXSpline Encoder [" + encoder.getDeviceId() + "]";
+    setupSimDevice();
+  }
+
+  // internal setup helper
+  private boolean setupSimDevice() {
+    SimDeviceSim detachedEncoderSim = new SimDeviceSim(simDeviceName);
+    m_position = detachedEncoderSim.getDouble("Position");
+    m_velocity = detachedEncoderSim.getDouble("Velocity");
+    m_angle = detachedEncoderSim.getDouble("Angle");
+    m_rawAngle = detachedEncoderSim.getDouble("Raw Angle");
+    m_isInverted = detachedEncoderSim.getBoolean("Is Inverted");
+    m_zeroOffset = detachedEncoderSim.getDouble("Zero Offset");
+    m_positionConversionFactor = detachedEncoderSim.getDouble("Position Conversion Factor");
+    m_velocityConversionFactor = detachedEncoderSim.getDouble("Velocity Conversion Factor");
+
+    return m_position == null;
+  }
+
+  // internal setup helper
+  private boolean checkAndSetupSimDevice() {
+    if (m_position == null) {
+      return setupSimDevice();
+    }
+    return false;
+  }
+
+  /**
+   * Set the relative position of the sensor, after your conversion factor
+   *
+   * @param position the position to set
+   */
+  public void setPosition(double position) {
+    if (checkAndSetupSimDevice()) return;
+    m_position.set(position);
+  }
+
+  /**
+   * Get the relative position of the sensor, with the conversion factor applied
+   *
+   * @return the position of the sensor
+   */
+  public double getPosition() {
+    if (checkAndSetupSimDevice()) return 0;
+    return m_position.get();
+  }
+
+  /**
+   * Set the velocity of the sensor, after the conversion factor
+   *
+   * @param velocity the velocity to set
+   */
+  public void setVelocity(double velocity) {
+    if (checkAndSetupSimDevice()) return;
+    m_velocity.set(velocity);
+  }
+
+  public void setAngle(double angle) {
+    if (checkAndSetupSimDevice()) return;
+    m_angle.set(angle);
+  }
+
+  public void setRawAngle(double rawAngle) {
+    if (checkAndSetupSimDevice()) return;
+    m_rawAngle.set(rawAngle);
+  }
+
+  /**
+   * Get the velocity of the sensor, with the conversion factor applied
+   *
+   * @return the velocity of the sensor
+   */
+  public double getVelocity() {
+    if (checkAndSetupSimDevice()) return 0;
+    return m_velocity.get();
+  }
+
+  /**
+   * Set the inversion state of the sensor
+   *
+   * @param inverted if the sensor is inverted or not
+   */
+  public void setInverted(boolean inverted) {
+    if (checkAndSetupSimDevice()) return;
+    m_isInverted.set(inverted);
+  }
+
+  /**
+   * Get the inversion state of the sensor
+   *
+   * @return if the sensor is inverted or not
+   */
+  public boolean getInverted() {
+    if (checkAndSetupSimDevice()) return false;
+    return m_isInverted.get();
+  }
+
+  /**
+   * Set the zero offset of the sensor
+   *
+   * @param zeroOffset the zero offset to apply
+   */
+  public void setZeroOffset(double zeroOffset) {
+    if (checkAndSetupSimDevice()) return;
+    m_zeroOffset.set(zeroOffset);
+  }
+
+  /**
+   * Get the zero offset of the sensor
+   *
+   * @return the zero offset of the sensor
+   */
+  public double getZeroOffset() {
+    if (checkAndSetupSimDevice()) return 0;
+    return m_zeroOffset.get();
+  }
+
+  /**
+   * Get the position conversion factor of the sensor (1 by default)
+   *
+   * @return the conversion factor
+   */
+  public double getPositionConversionFactor() {
+    if (checkAndSetupSimDevice()) return 0;
+    return m_positionConversionFactor.get();
+  }
+
+  /**
+   * Get the velocity conversion factor of the sensor (1 by default)
+   *
+   * @return the conversion factor
+   */
+  public double getVelocityConversionFactor() {
+    if (checkAndSetupSimDevice()) return 0;
+    return m_velocityConversionFactor.get();
+  }
+
+  /**
+   * Move the sensor at the input velocity
+   *
+   * @param velocity the velocity of the sensor
+   * @param dt the time interval of the calculation
+   */
+  public void iterate(double velocity, double dt) {
+    if (checkAndSetupSimDevice()) return;
+    double velocityRPM = velocity / getVelocityConversionFactor();
+    m_position.set(m_position.get() + ((velocityRPM / 60) * dt) * getPositionConversionFactor());
+    m_velocity.set(velocity);
+    m_angle.set(m_position.get());
+    m_rawAngle.set(m_position.get());
+  }
+}
