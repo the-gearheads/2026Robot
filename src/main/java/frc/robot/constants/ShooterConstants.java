a72de3637c76f588756cc41e79ef11f3b4f88dcb
Lolli-Pop7 <150016052@gpschools.org>
2026-03-12 18:06:57 -0400

    Assorted pathplanner stuff, added some of the commands needed to do other things than just drive

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -70,4 +70,7 @@ public class ShooterConstants {
 
     public static final Translation3d CENTER_BOT_TOSHOOT = new Translation3d(Units.inchesToMeters(-7.5572), Units.inchesToMeters(-9.2307), Units.inchesToMeters(20.5469));
 
+    public static final double DEPOT_TRENCH_SHOOT_RPM = 5000; //MUST ACTUALLY INPUT VALUE THIS IS A PLACEHOLDER
+    public static final Rotation2d DEPOT_TRENCH_SHOOT_ANGLE = new Rotation2d(30); //MUST ACTUALLY INPUT VALUE THIS IS A PLACEHOLDER
+
 }
