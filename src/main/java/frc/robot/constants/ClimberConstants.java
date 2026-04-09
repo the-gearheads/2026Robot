c4c7ab691b43b6b3e234212c29c9f9884d71c357
Gavin <Gavinoperry9@gmail.com>
2026-04-09 17:51:59 -0400

    added canandcolor

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -6,6 +6,10 @@ public class ClimberConstants {
     public static final int COLOR_SENSOR_ID = 51;
     public static final int CLIMBER_CURRENT_LIMIT = 60;
 
+    public static final double CLIMBING_POLE_PROXIMITY_THRESHOLD = 100.0;  // TODO: Tune this value based on actual sensor readings when near the climbing pole
+    public static final double CLIMB_POLE_MIN_HUE = 0.1;  
+    public static final double CLIMB_POLE_MAX_HUE = 0.18;  
+
     public static final double CLIMB_UP_VOLTAGE = 12.0;
     public static final double CLIMB_DOWN_VOLTAGE = 12.0;
 
--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.climber;
 
+import static frc.robot.constants.ClimberConstants.*;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
 
@@ -13,17 +15,30 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 
+import com.reduxrobotics.sensors.canandcolor.Canandcolor;
+import com.reduxrobotics.sensors.canandcolor.CanandcolorSettings;
+import com.reduxrobotics.sensors.canandcolor.DigoutFrameTrigger;
+import com.reduxrobotics.sensors.canandcolor.HSVDigoutConfig;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 
 public class Climber extends SubsystemBase {
-    SparkFlex climber = new SparkFlex(50,MotorType.kBrushless);
+    SparkFlex climber = new SparkFlex(CLIMBER_ID,MotorType.kBrushless);
+    Canandcolor canandcolor = new Canandcolor(COLOR_SENSOR_ID);
     RelativeEncoder climbEncoder = climber.getEncoder();
+
+    CanandcolorSettings settings = new CanandcolorSettings();
     SparkFlexConfig climbConfig = new SparkFlexConfig();
 
     public Climber() {
         configure();
         climbEncoder.setPosition(0);
+        canandcolor.digout1().configureSlots(new HSVDigoutConfig()
+                                .setMinProximity(0.0)
+                                .setMaxProximity(CLIMBING_POLE_PROXIMITY_THRESHOLD) // Trigger when closer than this
+                                .setMinHue(CLIMB_POLE_MIN_HUE)      
+                                .setMaxHue(CLIMB_POLE_MAX_HUE)
+        );
     }
 
     @Override
@@ -34,7 +49,7 @@ public class Climber extends SubsystemBase {
     }
     
     public void configure(){
-        climbConfig.smartCurrentLimit(60);
+        climbConfig.smartCurrentLimit(CLIMBER_CURRENT_LIMIT);
         climbConfig.idleMode(IdleMode.kBrake);
         climbConfig.inverted(false);
         climbConfig.softLimit.forwardSoftLimit(ClimberConstants.MAX_CLIMBER_POS);
@@ -42,6 +57,17 @@ public class Climber extends SubsystemBase {
         climbConfig.softLimit.reverseSoftLimitEnabled(true);
         climbConfig.softLimit.forwardSoftLimitEnabled(true);
         climber.configure(climbConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        
+
+        settings.setProximityFramePeriod(0.02);
+        settings.setColorFramePeriod(0.02);
+
+        // Tell the sensor to send a frame IMMEDIATELY when the logic trips
+        settings.setDigoutFrameTrigger(canandcolor.digout1().channelIndex(),
+                                    DigoutFrameTrigger.kRisingAndFalling);
+
+        canandcolor.setSettings(settings);
 
         climbConfig.encoder.quadratureMeasurementPeriod(10);
         climbConfig.encoder.quadratureAverageDepth(2); 
@@ -80,5 +106,9 @@ public class Climber extends SubsystemBase {
 
     public void setPosition(double position) {
         climbEncoder.setPosition(position);
+    }
+
+    public boolean climbingPole() {
+        return canandcolor.digout1().getValue();
     }
 }
