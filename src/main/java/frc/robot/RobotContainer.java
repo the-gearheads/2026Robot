59f82f87c37d002e6bc452dfdfcdd5aac699e537
Ceticx0 <gavinoperry9@gmail.com>
2026-04-11 18:38:33 -0400

    Revert "autoclimbs in auton???"
    This reverts commit 854e682b5b311ca02b638dd68d55a0dcc3a3f58c.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -338,6 +338,5 @@ public class RobotContainer {
               return ShooterCalculations.readyToShoot(swerve.getPose(), hood, shooter, ObjectiveTracker.HUB);
             }).withTimeout(5),
             spindexer.runSpindexer(12).alongWith(deploy.shimmy(intake)))).until(shooter::shouldIGo));
-      NamedCommands.registerCommand( "AutoClimb", climber.autoClimb(swerve));
   }
 }
